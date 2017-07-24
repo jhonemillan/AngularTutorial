a8a255587c4ac763bbb6cc45f9c4085f811767cf
jhon edwin millan <jhonemillan@gmail.com>
2017-07-23 23:47:01 -0500

    detalles

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,9 +8,15 @@ import { Recipe } from "app/recipes/recipe.model";
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes: Recipe[] = [ new Recipe("estofado", "Colombia food", "https://cdn.pixabay.com/photo/2017/04/04/00/36/japanese-food-2199962_960_720.jpg"),
-                        new Recipe("arepas fritas", "Arepas fritas", "http://www.venezuelatuya.com/cocina/imagenes/arepas_fritas.jpg"),
-                        new Recipe("Bandeja Paisa", "Frijoles, arroz, carne molida y mas", "http://cdn.colombia.com/sdi/2011/08/02/bandeja-paisa-500927.jpg")];
+  recipes: Recipe[] = [ new Recipe("estofado", 
+                                   "Colombia food", 
+                                   "https://cdn.pixabay.com/photo/2017/04/04/00/36/japanese-food-2199962_960_720.jpg"),
+                        new Recipe("arepas fritas", 
+                                   "Arepas fritas", 
+                                   "http://www.venezuelatuya.com/cocina/imagenes/arepas_fritas.jpg"),
+                        new Recipe("Bandeja Paisa", 
+                                   "Frijoles, arroz, carne molida y mas", 
+                                   "http://cdn.colombia.com/sdi/2011/08/02/bandeja-paisa-500927.jpg")];
   constructor() { }
 
   ngOnInit() {
