a3b6da9e88b414d5e88f1a704ebc0865d96fe6ec
jhon edwin millan <jhonemillan@gmail.com>
2017-07-24 23:55:23 -0500

    se habilitan los ingredientes

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../Shared/ingredient.model'
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+  ingredients: Ingredient[] = [
+    new Ingredient("Frijoles", 1),
+    new Ingredient("Arroz", 2)
+  ];
+  
   constructor() { }
 
   ngOnInit() {
